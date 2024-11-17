d0f4a45b96bdc2294472b9c60eb45cdda60944ed
Naman Sharma <naman.ns23@gmail.com>
2024-11-17 19:04:40 +0530

    Added navigation and data

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
+import { useEffect } from "react";
 import "./index.css";
 import Canvas from "./Canvas";
+import data from "./data";
+import LocomotiveScroll from 'locomotive-scroll';
 
 function App() {
-  return <>
-    <div className="w-full h-screen bg-black text-white">
-      <Canvas startIndex={0} />
-      <Canvas startIndex={150} />
-      <Canvas startIndex={300} />
-    </div>
-  </>
+  useEffect(() => {
+    const locomotiveScroll = new LocomotiveScroll();
+  }, []);
+
+  return (
+    <>
+      <div className="w-full relative min-h-screen text-white">
+        {data[0].map((canvasdets, index) => (
+          <Canvas key={index} details={canvasdets} />
+        ))}
+        <div className="w-full h-screen text-white">
+          <nav className="fixed top-0 left-0 w-full p-8 flex justify-between z-50">
+          </nav>
+          
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/Canvas.jsx
+++ src/Canvas.jsx
@@ -4,14 +4,15 @@ import canvasImages from "./canvasImages";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 
-function Canvas({ startIndex }) {
+function Canvas({ details }) {
+    const { startIndex, numImages, duration, size, top, left, zIndex } = details;
     const [index, setIndex] = useState({ value: startIndex });
     const canvasRef = useRef(null);
 
     useGSAP(() => {
         gsap.to(index, {
-            value: startIndex + 149,
-            duration: 3,
+            value: startIndex + numImages - 1,
+            duration: duration,
             ease: "linear",
             repeat: -1,
             onUpdate: () => {
@@ -21,23 +22,40 @@ function Canvas({ startIndex }) {
     });
 
     useEffect(() => {
+        const scale = window.devicePixelRatio;
         const canvas = canvasRef.current;
         const ctx = canvas.getContext('2d');
         const img = new Image();
         img.src = canvasImages[index.value];
         img.onload = () => {
-            canvas.width = img.width;
-            canvas.height = img.height;
-            ctx.drawImage(img, 0, 0);
+            canvas.width = canvas.offsetWidth * scale;
+            canvas.height = canvas.offsetHeight * scale;
+            canvas.style.width = canvas.offsetWidth + 'px';
+            canvas.style.height = canvas.offsetHeight + 'px';
+            ctx.scale(scale, scale);
+            ctx.drawImage(img, 0, 0, canvas.offsetWidth, canvas.offsetHeight);
         };  
     }, [index]);
 
-    return ( <canvas ref={canvasRef} className="w-[18rem] h-[18rem]" id="canvas"></canvas>
+    return ( 
+    <canvas
+        data-scroll
+        data-scroll-speed={Math.random().toFixed(1)}
+        ref={canvasRef}
+        className="absolute" 
+        style={{
+            width: `${size*1.8}px`, 
+            height: `${size*1.8}px`, 
+            top: `${top}%`, 
+            left: `${left}%`, 
+            zIndex: zIndex}}
+        id="canvas">
+    </canvas>
     );
 }
 
 Canvas.propTypes = {
-    startIndex: PropTypes.number.isRequired,
+    details: PropTypes.object.isRequired,
 };
 
 export default Canvas;
